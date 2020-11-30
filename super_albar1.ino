8de0c77be13ed4e81c00fc3e59e2630296a71811
abhishekm23 <72003055+abhishekm23@users.noreply.github.com>
2020-11-30 16:32:33 +0530

    Add files via upload

---
+++ super_albar1.ino
@@ -0,0 +1,41 @@
+//TUrn on and off external LED//
+
+void setup()
+{  
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  
+}
+void loop()
+{ 
+  digitalWrite(2,HIGH);
+  delay(2000);
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+   digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+  digitalWrite(4,HIGH);
+  delay(5000); 
+} 
+
+
+
+ 
